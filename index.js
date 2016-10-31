a03025351c39d1ffee98ce0f7577a5969d672477
kounelios13 <pkounelios@gmail.com>
2016-10-31 14:05:20 +0200

    updated package.json

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 #!/usr/bin/env node
 var log  = (o)=>console.log(o);
 log("Hello from arrow");
+var program = require("commander");
+program
+	.arguments("<file>")
+	.option("-i,--input"," The file to remove blank lines from")
